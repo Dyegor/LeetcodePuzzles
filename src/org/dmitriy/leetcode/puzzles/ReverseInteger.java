a42a33b20210593f95cfdde1fd571ec341144cc5
Dmitriy <dmitriy.yegorov85@gmail.com>
2021-01-12 17:32:17 +1300

    Reverse Integer solution

---
+++ src/org/dmitriy/leetcode/puzzles/ReverseInteger.java
@@ -0,0 +1,24 @@
+package org.dmitriy.leetcode.puzzles;
+
+public class ReverseInteger {
+    public static int reverse(int x) {
+        if (x == 0 || x == Integer.MIN_VALUE) {
+            return 0;
+        }
+
+        long res = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+            x /= 10;
+
+            res = res * 10 + digit;
+
+            if (res > Integer.MAX_VALUE || res < Integer.MIN_VALUE) {
+                return 0;
+            }
+        }
+
+        return (int) res;
+    }
+}
